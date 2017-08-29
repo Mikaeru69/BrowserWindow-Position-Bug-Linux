aa32582988000190d3303f91796759befc7c2639
Mikaeru69 <31444539+Mikaeru69@users.noreply.github.com>
2017-08-29 19:09:20 +0200

    Tab -> 4 Spaces

--- main.js
+++ main.js
@@ -8,21 +8,21 @@ let mainWindow = null;
 
 function onAppReady ()
 {
-	let position = [ 200, 100 ];
+    let position = [ 200, 100 ];
 
-	console.log ('init:', position);
+    console.log ('init:', position);
 
-	mainWindow = new BrowserWindow ({ x: position[0], y: position[1], width: 800, height: 600, show: false });
+    mainWindow = new BrowserWindow ({ x: position[0], y: position[1], width: 800, height: 600, show: false });
 
-	console.log ('new:', mainWindow.getPosition ());
+    console.log ('new:', mainWindow.getPosition ());
 
-	mainWindow.loadURL (`file://${__dirname}/index.html`);
+    mainWindow.loadURL (`file://${__dirname}/index.html`);
 
-	mainWindow.on ('ready-to-show', () => { mainWindow.show (); console.log ('show:', mainWindow.getPosition ()); });
+    mainWindow.on ('ready-to-show', () => { mainWindow.show (); console.log ('show:', mainWindow.getPosition ()); });
 
-	mainWindow.on ('close', () => { console.log ('close:', mainWindow.getPosition ()); });
+    mainWindow.on ('close', () => { console.log ('close:', mainWindow.getPosition ()); });
 
-	mainWindow.on ('closed', () => { app.quit (); });
+    mainWindow.on ('closed', () => { app.quit (); });
 }
 
 app.on ('ready', onAppReady);
